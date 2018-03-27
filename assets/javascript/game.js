8b17dbb23919b3283f380bcb7ad7c74d6f85666c
Austin Wagner <austin.e.wagner@gmail.com>
2018-03-27 11:53:22 -0700

    word bank and variables created

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+let correct = 0;
+let wrong = 0;
+let currentWord = "";
+let wordArray = [wordBank.w1, wordBank.w2, wordBank.w3, wordBank.w4, wordBank.w5, wordBank.w6, wordBank.w7, wordBank.w8, wordBank.w9, wordBank.w10];
+
+//Create Word Bank
+const wordBank = {
+    w1: "Finn",
+    w2: "Jake",
+    w3: "Princess Bubblegum",
+    w4: "Marceline the Vampire Queen",
+    w5: "BMO",
+    w6: "Lemongrab",
+    w7: "Ice King",
+    w8: "Magic Man",
+    w9: "Prismo",
+    w10: "The Lich"
+}
+
+//Game setup
+
+function renderGame() {
+
+}
+
+// Create logic for choosing random wordBank word
+
+// Check the length of word and render dashes
+// Check for spaces/special characters
+
+
+
+
+//Create event listener
+
+//Check for letter
+
+//
